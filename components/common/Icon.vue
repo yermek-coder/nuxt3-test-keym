0662160bbe603ff3fd5d4fbfb7c112ad300090ec
Yermek Orazgaliyev <yermek.orazgaliyev@smartgroups.io>
2025-02-24 20:33:28 +0500

    Icon component added

---
+++ components/common/Icon.vue
@@ -0,0 +1,10 @@
+<template>
+    <i :class="style"></i>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps({ icon: String });
+const style = computed(() => `icon bi bi-${props.icon}`);
+</script>
