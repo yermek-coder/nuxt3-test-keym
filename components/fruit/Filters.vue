94ea31184f37ed79c75385524a9d26ec4b451d06
Yermek Orazgaliyev <yermek.orazgaliyev@smartgroups.io>
2025-02-24 20:29:16 +0500

    Added filters

---
+++ components/fruit/Filters.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="fruit-filters">
+        <div v-for="(value, key) in filters" :key="key" class="fruit-filters-group">
+            <div class="fruit-filters-group-title">{{ key }}</div>
+            <div class="fruit-filters-group-body">
+                <input v-model="value[0]" type="number">
+                <input v-model="value[1]" type="number">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { FruitFilters } from '~/types/fruit';
+defineProps<{ filters: FruitFilters }>()
+</script>
